54f1db9b5f9a5cee35c26f7c5e717ad1d85ca3b9
pliniocanto <pliniocanto@gmail.com>
2024-05-26 13:25:35 -0300

    first commit

---
+++ src/test/java/com/example/pmac/dynamoaws/DynamoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmac.dynamoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
